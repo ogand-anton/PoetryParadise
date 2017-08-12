ba7cea3f6edc166ba7ca5ec292c3c34ea45b5980
Anton Ogandzhanyan <ogandzhanyan.a@husky.neu.edu>
2017-08-11 20:42:50 -0400

    Tweaking Login w/ Google

--- models/user/schema_user.js
+++ models/user/schema_user.js
@@ -3,7 +3,7 @@ module.exports = function () {
 
     return mongoose.Schema({
         username: {type: String, required: true, trim: true, unique: true},
-        password: {type: String, required: true, select: false},
+        password: {type: String, select: false},
         firstName: {type: String},
         lastName: {type: String},
         emailAddress: {type: String},
