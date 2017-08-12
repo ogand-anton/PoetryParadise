560d27cc284978c1c9ce33e6712b238c3a20b3f8
Anton Ogandzhanyan <ogandzhanyan.a@husky.neu.edu>
2017-08-12 12:08:12 -0400

    File path tweaks

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ var bodyParser = require("body-parser"),
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.use(express.static(__dirname + "/public"));
 app.use(session({secret: "this is the secret", resave: true, saveUninitialized: true}));
-app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 app.listen(process.env.PORT || 3000);
